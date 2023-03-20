d9831d3e8d6d0bb65c8044c270acc0df9bb16df5
yingmu <wawamu2201@gmail.com>
2023-03-20 22:08:31 +0100

    skilspage header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
+import MySkills from "./scenes/MySkills";
+
+import LineGradient from "./components/LineGradient";
 
 import { useState, useEffect } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -33,9 +36,13 @@ function App() {
             setSelectedPage={setSelectedPage}
           />
         )}
-        
-          <Landing setSelectedPage={setSelectedPage} />
-        
+
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+
+      <div className="w-5/6 mx-auto md:h-full">
+          <MySkills />
       </div>
     </div>
   );
--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -11,27 +11,33 @@ const Landing = ({ setSelectedPage }) => {
       id="home"
       className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
     >
+
+
+
       {/* IMAGE SECTION */}
       <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveLarge ? (
           <div
-            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 
             before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               alt="profile"
-              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.png"
+              className="hover:filter hover:saturate-50 transition duration-500 z-10 h-[400px] w-full max-w-[400px] md:max-w-[600px]"
+              src="assets/ym.jpg"
             />
           </div>
         ) : (
           <img
             alt="profile"
             className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/profile-image.png"
+            src="assets/ym.jpg"
           />
         )}
       </div>
+
+
+
 
       {/* MAIN TEXT */}
       <div className="z-30 basis-2/5 mt-12 md:mt-32">
@@ -47,7 +53,7 @@ const Landing = ({ setSelectedPage }) => {
           }}
         >
           <p className="text-6xl font-playfair z-10 text-center md:text-start">
-            Yingmu {""}
+            Yingmu {""} 
             <span
               className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
               before:absolute before:-left-[35px] before:-top-[125px] before:z-[-1]"
